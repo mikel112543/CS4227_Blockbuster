8a0d7da4793d3a5b9cbae55eaae20bb1cbb903d7
Mikel112543 <michael.danaher112543@gmail.com>
2021-11-16 16:00:18 +0000

    Exception_Handling class

---
+++ src/main/java/com/example/movierental/handler/ExceptionController.java
@@ -0,0 +1,8 @@
+package com.example.movierental.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+}
