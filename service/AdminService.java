4373c57d516f895f3bda6a47c51f46b779ebb31a
aaron <18232086@studentmail.ul.ie>
2021-11-16 12:28:21 +0000

    Admin service, movie service. Search movie is Movie service

--- service/AdminService.java
+++ service/AdminService.java
@@ -1,41 +1,15 @@
 package service;
 
-import model.Movie;
+import model.User;
 
-import java.io.*;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class AdminService {
-
-    File moviesCSV = new File("Movies.csv");
-    File usersCSV = new File("Users.csv");
-
-
-    public void addMovie(String title, String genre, String description, LocalTime movieLength, LocalTime rentLength, double price, int movieID, int movieRating, String movieRelease) throws IOException {
-        Movie movie = new Movie(title, genre, description, movieLength, rentLength, price, movieID, movieRating, movieRelease);
-
-        PrintWriter pw = new PrintWriter(moviesCSV);
-
-        pw.println(movie.getTitle() + "," + movie.getGenre() + "," + movie.getDescription() + "," + movie.getMovieLength() + "," + movie.getRentLength() + "," + movie.getPrice() + "," + movie.getMovieID() + "," + movie.getMovieRating() + "," + movie.getMovieRelease());
-        pw.close();
-    }
-
-    public void deleteMovie(int movieID) {
-        //TODO: search the CSV file for the movieID (column 7) and remove the entire row.
-        boolean found = false;
-
-    }
-
-    public ArrayList<User> listAllUsers(){
-        //TODO: put each user into an arraylist of users. then return this arraylist.
-
-
-        //copy 1st row of csv file, turn into object, add object to arraylist. repeat for each row of the csv file.
-        return listOfUsers;
-    }
-
-    public void banUser(int userID){
-
-    }
+public interface AdminService {
+    public abstract void addMovie(String title, String genre, String description, LocalTime movieLength, LocalTime rentLength, double price, int movieID, int movieRating, String movieRelease) throws IOException;
+    //public abstract void deleteMovie(int movieID);
+    public abstract ArrayList<User> listAllUsers() throws IOException;
+    //public Abstract User createUser(String [] metadata);
+    public abstract void banUser(int userID);
 }
