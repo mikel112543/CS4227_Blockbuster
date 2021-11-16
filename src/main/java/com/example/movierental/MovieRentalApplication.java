f6aa158cd980936313ba28caac77c7bdfc4dbaa1
Mikel112543 <michael.danaher112543@gmail.com>
2021-11-16 02:04:14 +0000

    Exception_Handling class

--- src/main/java/com/example/movierental/MovieRentalApplication.java
+++ src/main/java/com/example/movierental/MovieRentalApplication.java
@@ -1,5 +1,6 @@
 package com.example.movierental;
 
+import com.example.movierental.constants.Error;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
