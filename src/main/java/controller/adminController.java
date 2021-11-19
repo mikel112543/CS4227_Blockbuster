e155696edab1927c5e4bd47a3286027ac65bbdc6
Mikel112543 <michael.danaher112543@gmail.com>
2021-11-19 17:04:54 +0000

    Revert "- Updating Controllers to use MVC"
    This reverts commit 2bdb9e20

--- src/main/java/controller/adminController.java
+++ src/main/java/controller/adminController.java
@@ -1,46 +1,51 @@
 package controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
-@Controller //CRUD
+@RestController //CRUD
 public class adminController {
 
     /**
      * @param userId - Unique identifier attached to each of the users.
      * @return User - Returns object of type user.
      */
-    @PostMapping(value = "admin/customerId/{CUSTOMER_ID}")
-    public String banCustomer(@PathVariable("USER_ID") final int customerId) {
-        //Service Call
-        adminService.deleteUser(customerId);
-        return "adminMenu";
+    @DeleteMapping(value = "admin/customerId/{CUSTOMER_ID}")
+    public User banCustomer(@PathVariable("USER_ID") final int customerId) {
+        return adminService.deleteUser(customerId);
+    }
+
+    /**
+     * @param movieTitle       - Title of the movie to be added
+     * @param movieGenre       - Genre of the movie to be added
+     * @param movieDescription - Short description of the movie to the added
+     * @param movieLength      - Length of movie to be added
+     * @param moviePrice       - Starting price of movie to be added
+     * @return - Json response
+     */
+    @PostMapping(value = "admin/movieTitle/{MOVIE_TITLE}/movieGenre/{MOVIE_GENRE}/movieDescription/{MOVIE_DESCRIPTION}/movieLength/{MOVIE_LENGTH}/moviePrice/{MOVIE_PRICE}")
+    public User addMovie(@PathVariable("MOVIE_TITLE") final String movieTitle,
+                          @PathVariable("MOVIE_GENRE") final String movieGenre,
+                          @PathVariable("MOVIE_DESCRIPTION") String movieDescription,
+                          @PathVariable("MOVIE_LENGTH") final double movieLength,
+                          @PathVariable("MOVIE_PRICE") double moviePrice) {
+        return adminService.addMovie(movieTitle, movieGenre, movieDescription,
+                movieLength, moviePrice);
     }
 
     /**
      * @param movieId - Unique identifier attached to each of the movies.
      * @return boolean - Returns boolean if movie was correctly removed.
      */
-    @DeleteMapping(value = "admin/MovieIdDelete/{MOVIE_ID}")
-    public String deleteMovie(@PathVariable("MOVIE_ID") final int movieId) {
-        //Service Call
-        adminService.deleteMovie(movieId);
-        return "adminMenu";
+    @DeleteMapping(value = "admin/MovieIdDelete/{movieId}")
+    public User deleteMovie(@PathVariable("movieId") final int movieId) {
+        return adminService.deleteMovie(movieId);
     }
 
     /**
      * @return returns all users
      */
-    @GetMapping(value = "/adminMenu")
-    public String showAdminMenuView(Model userModel, Model movieModel) {
-        //Service Call
-        ArrayList<User> Users = adminService.listAllUsers();
-        ArrayList<Movie> Movies = movieService.listAllMovies();
-        userModel.addAttribute("users", users);
-        movieModel.addAttribute("movies", movies);
-
-        return "adminMenu";
+    @GetMapping(value = "admin/users")
+    public User listAllUsers() {
+        return adminService.listAllUsers();
     }
 }
--- src/main/java/controller/movieController.java
+++ src/main/java/controller/movieController.java
@@ -1,42 +1,34 @@
 package controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller //CRUD
+@RestController //CRUD
 public class movieController {
 
     /**
      * @return List of Movies
      */
     @GetMapping(value = "/movies")
-    public String listMoviesView(Model model) {
-        //Service Call
-        ArrayList<Movie> movies = movieService.listAll();
-        model.addAttribute("movies", movies);
-        return "movies";
+    public Movie listAll() {
+        return movieService.listAll();
     }
 
     /**
      * @param movieId - Unique identifier attached to each of the movies.
+     * @return JSON Movie
      */
     @GetMapping(value = "movies/MovieId/{MOVIE_ID}")
-    public String getMovie(@PathVariable("MOVIE_ID") final int movieId) {
-        //Service Call
-        return movieService.findById(movieId); // Send request to movie service handler
+    public Movie getMovie(@PathVariable("MOVIE_ID") final int movieId) {
+        return movieService.getMovie(movieId); // Send request to movie service handler
     }
 
     /**
      *
      * @param movieName - name of movie to be searched
+     * @return JSON Movie
      */
     @GetMapping(value = "/movies/movieName/{MOVIE_NAME}")
-    public String searchMovie(@PathVariable("MOVIE_NAME") String movieName,
-                              Model model) {
-        //Service Call
-        ArrayList<Movie> movies = movieService.findByName(movieName);
-        model.addAttribute("movies", movies);
-        return "MovieSearch";
+    public Movie searchMovie(@PathVariable("MOVIE_NAME") String movieName) {
+        return movieService.searchMovie(movieName);
     }
 }
