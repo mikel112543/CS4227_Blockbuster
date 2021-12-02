fe8834f14c07b9cac3f5262ebe75ddcf22686624
Michael Danaher <mikel.d@live.ie>
2021-12-02 00:28:29 +0000

    - Fully functional Movies Html page - Fully working rental button

--- src/main/java/com/example/movierental/controller/MovieController.java
+++ src/main/java/com/example/movierental/controller/MovieController.java
@@ -3,8 +3,10 @@ package com.example.movierental.controller;
 import com.example.movierental.model.Movie;
 import com.example.movierental.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-@RestController //CRUD
+@Controller //CRUD
 public class MovieController {
 
     @Autowired
@@ -13,16 +15,18 @@ public class MovieController {
     /**
      * @return List of Movies
      */
-/*    @GetMapping(value = "/movies")
-    public Movie listAll() {
-        return movieService.listAllMovies();
-    }*/
+    @GetMapping(value = "/movies")
+    public String showMovies(Model model) {
+        model.addAttribute("movies", movieService.getMovies());
+        return "movies";
+    }
 
     /**
      * @param movieId - Unique identifier attached to each of the movies.
      * @return JSON Movie
      */
     @GetMapping(value = "movies/{MOVIE_ID}")
+    @ResponseBody
     public Movie getMovie(@PathVariable("MOVIE_ID") final int movieId) {
         return movieService.findByMovieID(movieId); // Send request to movie service handler
     }
@@ -32,7 +36,7 @@ public class MovieController {
      * @param movieName - name of movie to be searched
      * @return JSON Movie
      */
-/*    @GetMapping(value = "/movies/search/{MOVIE_NAME}")
+ /* @GetMapping(value = "/movies/search/{MOVIE_NAME}")
     public Movie searchMovie(@PathVariable("MOVIE_NAME") String movieName) {
         return movieService.searchMovie(movieName);
     }*/
