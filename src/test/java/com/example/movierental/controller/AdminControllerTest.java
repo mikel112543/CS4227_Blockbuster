cd2db6d4396caaec2daaee3e506de3bd33bdb4cb
aaron <18232086@studentmail.ul.ie>
2021-12-14 20:16:22 +0000

    MovieControllerTest done

--- src/test/java/com/example/movierental/controller/AdminControllerTest.java
+++ src/test/java/com/example/movierental/controller/AdminControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.movierental.controller;
 
+import com.example.movierental.model.Movie;
 import com.example.movierental.model.User;
 import com.example.movierental.service.AdminServiceImpl;
 import com.example.movierental.service.MovieServiceImpl;
 import com.example.movierental.service.RentalServiceImpl;
 import com.example.movierental.service.UserRepoServiceImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -47,20 +49,34 @@ class AdminControllerTest {
 
     @BeforeEach
     void setUp()  {
-        User testUser = new User(99, "test", "test", "ROLE_USER", false);
-        User testAdmin = new User(100, "admin", "admin", "ROLE_ADMIN", false);
+        User testUserUnbanned = new User(99, "test", "test", "ROLE_USER", false);
+        User testUserBanned = new User(99, "test", "test", "ROLE_USER", true);
 
+        User testAdmin = new User(100, "admin", "admin", "ROLE_ADMIN", false);
+        Movie movie1 = new Movie.MovieBuilder("The Dark Knight", "Action", "Batman and that", "2 hours", 99, "batmancoverphoto").setPrice(1).build();
+        Movie movie2 = new Movie.MovieBuilder("The return of the king", "Action", "Lord of the Rings", "2 hours", 100, "lordoftheringscoverphoto").setPrice(1).build();
+        movieService.addMovie(movie1);
+        movieService.addMovie(movie2);
+        userService.addUser(testUserUnbanned);
+        userService.addUser(testUserBanned);
+    }
+
+    @AfterEach
+    void tearDown(){
+        movieService.removeMovie();
     }
 
     @Test
     @DisplayName("POST to add a movie to the List of Movies")
     void addMovie() throws Exception {
-        mockMvc.perform(post("/admin/movieTitle/Transformers/movieGenre/action/movieDescription/robots and stuff/movieLength/2 hours/moviePrice/1/movieCoverUrl/transformers.jpg")
+        mockMvc.perform(post("/admin/movieTitle/Transformers/movieGenre/Action/movieDescription/Robots and Stuff/movieLength/2 hours/moviePrice/1/movieCoverUrl/transformers.jpg")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].Movie").exists())
-                .andExpect(jsonPath("$[0].Movie.Title", is("Transformers")))
-                .andExpect(jsonPath("$[0].Movie.Genre", is("Action")));
+                .andExpect(jsonPath("$.Movie.Title", is("Transformers")))
+                .andExpect(jsonPath("$.Movie.Genre", is("Action")))
+                .andExpect(jsonPath("$.Movie.Description", is("Robots and Stuff")))
+                .andExpect(jsonPath("$.Movie.Length", is("2 hours")));
+
     }
 
 
