8c144a858086f7683f3836f2df992dbb8652fc97
John Maguire <48322150+JohnMags99@users.noreply.github.com>
2021-12-14 00:38:05 +0000

    Possible build fix

--- src/main/java/com/example/movierental/states/Tier1.java
+++ src/main/java/com/example/movierental/states/Tier1.java
@@ -1,11 +1,5 @@
 package com.example.movierental.states;
 
-import com.example.movierental.model.Movie;
-import com.example.movierental.model.Rental;
-
-import javax.rmi.CORBA.Tie;
-import java.time.LocalDate;
-
 // Concrete State
 public class Tier1 implements Tier {
 
