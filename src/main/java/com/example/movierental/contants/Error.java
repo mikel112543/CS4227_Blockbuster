d8d514be604d8642abb6f3d920a6e00c4d3cd6fe
jackmurphy2018 <jackmurphy2018@gmail.com>
2021-12-14 11:02:23 +0000

    PriceFactoryTest class and error handling in PriceFactoryClass

--- src/main/java/com/example/movierental/contants/Error.java
+++ src/main/java/com/example/movierental/contants/Error.java
@@ -12,10 +12,11 @@ public enum Error {
     NO_RENTALS("2006", "User has no rentals"),
     ALREADY_RENTING("2007", "You are currently renting this movie"),
     INVALID_USER_RENTALS("2008", "Invalid Rentals"),
-    INVALID_MOVIE_NAME("2009", "Can't find movie with search, search for another movie isntead"),
+    INVALID_MOVIE_NAME("2009", "Can't find movie with search, search for another movie instead"),
     INVALID_LOGIN_CREDENTIALS("2010", "Login Credentials are Incorrect"),
     USER_LOAD_FAILURE("2011", "Failed to load user, Username may be invalid"),
-    FILE_NOT_FOUND("2012", "Could not find specified file");
+    FILE_NOT_FOUND("2012", "Could not find specified file"),
+    INVALID_PRICE_CODE("2013","Incorrect price code entered: Must be one of 0,1,2");
 
 
     private String errorCode;
--- src/main/java/com/example/movierental/model/PriceFactory.java
+++ src/main/java/com/example/movierental/model/PriceFactory.java
@@ -4,7 +4,16 @@ package com.example.movierental.model;
 //Implements the factory method
 //@author Jack Murphy - 18254268
 
+import com.example.movierental.contants.Error;
+import com.example.movierental.exception.ServiceException;
+import com.example.movierental.logger.AbstractLogger;
+import com.example.movierental.logger.RequesterClient;
+
+import java.rmi.server.ServerCloneException;
+
 public class PriceFactory {
+
+    private static AbstractLogger chainLogger = RequesterClient.getChaining();
 
     //getPrice returns the required Price object based on the priceCode
     //PRICE CODES: 0 = NewPrice, 1 = StandardPrice, 2 = ChildrensPrice
@@ -17,8 +26,8 @@ public class PriceFactory {
         }else if(priceCode == 2){
             return new ChildrensPrice();
         }else{
-            System.out.println("Enter error checking here");
+            chainLogger.logMessage(AbstractLogger.ERROR_INFO,"Incorrect price code entered: Must be one of 0,1,2");
+            throw new ServiceException(new ServiceError(Error.INVALID_PRICE_CODE));
         }
-        return null;
     }
 }
