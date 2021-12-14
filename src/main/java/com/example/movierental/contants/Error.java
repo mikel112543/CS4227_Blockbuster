07a8cffa4892781f8f5370596311f24cf6b72005
jackmurphy2018 <jackmurphy2018@gmail.com>
2021-12-14 11:02:23 +0000

    PriceFactoryTest class and error handling in PriceFactoryClass

--- src/main/java/com/example/movierental/contants/Error.java
+++ src/main/java/com/example/movierental/contants/Error.java
@@ -12,7 +12,8 @@ public enum Error {
     NO_RENTALS("2006", "User has no rentals"),
     ALREADY_RENTING("2007", "You are currently renting this movie"),
     INVALID_USER_RENTALS("2008", "Invalid Rentals"),
-    INVALID_MOVIE_NAME("2009", "Can't find movie with search, search for another movie isntead");
+    INVALID_MOVIE_NAME("2009", "Can't find movie with search, search for another movie isntead"),
+    INVALID_PRICE_CODE("2010","Incorrect price code entered: Must be one of 0,1,2");
 
 
     private String errorCode;
--- src/test/java/com/example/movierental/model/PriceFactoryTest.java
+++ src/test/java/com/example/movierental/model/PriceFactoryTest.java
@@ -8,15 +8,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PriceFactoryTest {
 
+    private Price p1, p2, p3;//p4;
+    private PriceFactory p;
+    /**
+     * priceCodes
+     * 0 = New Release
+     * 1 = Standard Release
+     * 2 = Childrens Release
+     */
     @BeforeEach
     void setUp() {
+        p = new PriceFactory();
+        p1 = p.getPrice(0);
+        p2 = p.getPrice(1);
+        p3 = p.getPrice(2);
+        //p4 = p.getPrice(4);
     }
 
     @AfterEach
     void tearDown() {
+        p = null;
+        p1 = null;
+        p2 = null;
+        p3 = null;
+        //p4 = null;
     }
 
     @Test
-    void getPrice() {
+    void TestGetPrice() {
+        assertTrue(p1 instanceof NewPrice);
+        assertTrue(p1 instanceof StandardPrice);
+        assertTrue(p3 instanceof ChildrensPrice);
     }
 }
